fa3d5cb4c48915b6a43a6751c26da62f06186e22
vlsk3104 <vlsk3104@google.com>
2022-08-17 15:18:20 +0900

    fix

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 const Stack = createNativeStackNavigator();
